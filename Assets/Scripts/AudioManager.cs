1be1e187096cc91994c78f198bbc9d834f3580e4
azizichwani <ichwaniaziz@gmail.com>
2022-08-29 16:34:09 +0800

    Optimize

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] private List<AudioSource> _bgmAudioSource;
     public bool isAudioEnabled = false;
-     private static AudioManager _instance;
+    private static AudioManager _instance;
 
     public static AudioManager Instance { get { return _instance; } }
     private void Awake() 
-    {   
+    {
+        _bgmAudioSource = Resources.LoadAll<AudioSource>("BGM").ToList();
+
        if (_instance != null && _instance != this)
         {
             Destroy(this.gameObject);
@@ -21,6 +24,12 @@ public class AudioManager : MonoBehaviour
     }
     public void PlayBgm()
     {
-        _bgmAudioSource[Random.Range(0, _bgmAudioSource.Count)].Play();
+        //_bgmAudioSource[Random.Range(0, _bgmAudioSource.Count)].Play();
+
+        int randomIndex = Random.Range(0, _bgmAudioSource.Count);
+        AudioSource randomBgm = Instantiate(_bgmAudioSource[randomIndex], transform);
+        randomBgm.Play();
+        _bgmAudioSource.Clear();
+        Resources.UnloadUnusedAssets();
     }
 }
--- Assets/Scripts/MushroomSpawner.cs
+++ Assets/Scripts/MushroomSpawner.cs
@@ -4,9 +4,58 @@ using UnityEngine;
 
 public class MushroomSpawner : MonoBehaviour
 {
-    public GameObject objPrefab;
-    
+    //public GameObject objPrefab;
+    [SerializeField] private GameObject _mushroom;
+    [SerializeField] private int _maxMushroom = 20;
+    [SerializeField] private List<GameObject> _mushroomSpawned;
+
+    private float timer;
+
+    private void Awake()
+    {
+        _mushroomSpawned = new List<GameObject>();
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < _maxMushroom; i++)
+        {
+            GameObject mushroom = Instantiate(_mushroom, transform.position, Quaternion.identity, transform);
+            mushroom.SetActive(false);
+            _mushroomSpawned.Add(mushroom);
+        }
+    }
+
     void FixedUpdate(){
-        Instantiate(objPrefab, transform.position, Quaternion.identity);
+        //Instantiate(objPrefab, transform.position, Quaternion.identity);
+
+        SpawnMushroomPool();
+    }
+
+    public void SpawnMushroomPool()
+    {
+        GameObject mushroomPool = MushroomPool();
+        timer += Time.deltaTime;
+        if(timer > 0.3)
+        {
+            if(mushroomPool != null)
+            {
+                mushroomPool.transform.position = this.transform.position;
+                mushroomPool.SetActive(true);
+            }
+            timer = 0;
+        }
+    }
+
+    public GameObject MushroomPool()
+    {
+        for ( int  i = 0; i < _mushroomSpawned.Count; i++ )
+        {
+            if(!_mushroomSpawned[i].activeInHierarchy)
+            {
+                return _mushroomSpawned[i];
+            }
+        }
+        return null;
     }
 }
